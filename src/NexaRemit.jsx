7cf2a4ec737471e866dc8ac4d069cf8b5c486799
pinchez28-cyber <pinchez28@gmail.com>
2026-05-22 15:57:38 -0400

    Remove review queue from production navigation

--- src/NexaRemit.jsx
+++ src/NexaRemit.jsx
@@ -16,7 +16,6 @@ import Setup from "./pages/Setup";
 import Integrations from "./pages/Integrations";
 import TransferHistory from "./pages/TransferHistory";
 import Receipt from "./pages/Receipt";
-import ReviewQueue from "./pages/ReviewQueue";
 
 const AuthenticatedApp = () => {
   const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();
@@ -45,7 +44,6 @@ const AuthenticatedApp = () => {
         <Route path="/Setup" element={<Setup />} />
         <Route path="/SendMoney" element={<SendMoney />} />
         <Route path="/History" element={<TransferHistory />} />
-        <Route path="/Reviews" element={<ReviewQueue />} />
         <Route path="/Receipt/:id" element={<Receipt />} />
         <Route path="/Recipients" element={<Recipients />} />
         <Route path="/Integrations" element={<Integrations />} />
